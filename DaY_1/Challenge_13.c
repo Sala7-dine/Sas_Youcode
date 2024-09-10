c425d7823b8b5b4bac2ef10ebe1d3f90f5cda6b4
salahdine <salahdaha7@gmail.com>
2024-09-10 12:49:49 +0200

    The Last Commit

--- DaY_1/Challenge_13.c
+++ DaY_1/Challenge_13.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+
 
 int main()
 {
     int n;
-    int Taille = 0 , i=0; 
+    int Taille=0, i=0; 
 
     printf("Saisir un nombre : ");
     scanf("%d" , &n);
 
+    int sign = n;
+    n = abs(n);
+
     int s = n;
     int hexa = n;
-
+   
     while(n!=0){   
         n = n / 2;
         Taille++;
@@ -25,10 +30,11 @@ int main()
         i++;
     } 
 
-    printf("Le Nombre en binaire est : ");
+    printf("Le Nombre en binaire est : \n");
+    if(sign < 0) printf("%d",1);
     for(int i=Taille-1;i>=0;i--)
         printf("%d" , T[i]);
 
-    printf("\nLe Nombre en Hexadicemal est : %x ",hexa);
+    printf("\nLe Nombre en Hexadicemal est : %X ",hexa);
 
 }
